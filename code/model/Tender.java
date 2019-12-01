5c4d2d004ee2b43aca993c3571f73053e72724a5
Wioletta Nosenko <violetta.nosenko@gmail.com>
2019-12-01 20:54:20 +0300

    counting price points added

--- code/model/Tender.java
+++ code/model/Tender.java
@@ -3,6 +3,9 @@ package model;
 import java.util.ArrayList;
 
 public class Tender {
+    static final double BEST_POINT = 10;
+    static final double WORST_POINT = 1;
+
     private String name;
     private String product;
     private double kPrice;
@@ -12,8 +15,30 @@ public class Tender {
     private TenderMember winner1;
     private TenderMember winner2;
 
-    public void countPricePoints() {
+    public ArrayList<Double> countPricePoints() {
+        ArrayList<Double> priceList = new ArrayList<>();
+        for (TenderMember tM:tenderMembers){
+            priceList.add(tM.getPrice());
+        }
+        double bestPrice = priceList.get(0);
+        double worstPrice = bestPrice;
+        for (double price:priceList){
+            if (price<bestPrice) bestPrice = price;
+            if (price>worstPrice) worstPrice = price;
+        }
+        ArrayList<Double> pointsList = new ArrayList<>();
+        double delta = worstPrice - bestPrice;
 
+        for (double price:priceList){
+            if (price == bestPrice) pointsList.add(BEST_POINT);
+            else if(price == worstPrice) pointsList.add(WORST_POINT);
+            else {
+                double point = ((worstPrice - price)/delta)*10 + WORST_POINT;
+                pointsList.add(point);
+            }
+        }
+
+        return pointsList;
     }
 
     public void countDaysPoints() {
