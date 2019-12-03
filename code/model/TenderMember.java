5c0f4d1e114a4bad1f5a3aa696e83eaad7a7caa6
Wioletta Nosenko <violetta.nosenko@gmail.com>
2019-12-03 04:23:13 +0300

    getters & setters ready

--- code/model/TenderMember.java
+++ code/model/TenderMember.java
@@ -11,33 +11,60 @@ public class TenderMember {
     private String name;
     private Double price=0.0;
     private DoubleProperty pricePoint;
-    private double priceKPoint;
-    private int days;
-    private double daysPoint;
-    private double daysKPoint;
+    private DoubleProperty priceKPoint;
+    private Integer days=0;
+    private DoubleProperty daysPoint;
+    private DoubleProperty daysKPoint;
     private int prePayPrecent;
     private int prePayDays;
     private int factPayPrecent;
     private int factPayDays;
-    private double conditionPoint;
-    private double conditionKPoint;
+    private DoubleProperty conditionPoint;
+    private DoubleProperty conditionKPoint;
+    private DoubleProperty generalPoint;
 
     public TenderMember()
     {
         countOfMembers++;
         index = countOfMembers;
+
         pricePoint = new SimpleDoubleProperty(0);
+        priceKPoint = new SimpleDoubleProperty(0);
+        daysPoint = new SimpleDoubleProperty(0);
+        daysKPoint = new SimpleDoubleProperty(0);
+        conditionKPoint = new SimpleDoubleProperty(0);
+        conditionPoint = new SimpleDoubleProperty(0);
+        generalPoint = new SimpleDoubleProperty(0);
     }
 
     public int getIndex() { return index; }
     public String getName() { return name; }
     public String getPrice() { return price.toString(); }
-    public Double getPricePoint() { return pricePoint.get(); }
-    public DoubleProperty pricePointProperty() { return pricePoint;}
-    public double getPriceKPoint() { return priceKPoint; }
-    public int getDays() { return days; };
-    public double getDaysPoint() { return daysPoint; }
-    public double getDaysKPoint() { return daysKPoint; }
+    public double getPricePoint() { return pricePoint.get(); }
+    public double getPriceKPoint() { return priceKPoint.get(); }
+    public String getDays() { return days.toString(); };
+    public double getDaysPoint() { return daysPoint.get(); }
+    public double getDaysKPoint() { return daysKPoint.get(); }
+    public double getConditionPoint() { return conditionPoint.get(); }
+    public double getConditionKPoint() { return conditionKPoint.get(); }
+    public double getGeneralPoint() { return generalPoint.get(); }
+
+    public DoubleProperty pricePointProperty() { return pricePoint; }
+    public DoubleProperty priceKPointProperty() { return priceKPoint; }
+    public DoubleProperty daysPointProperty() { return daysPoint; }
+    public DoubleProperty daysKPointProperty() { return daysKPoint; }
+    public DoubleProperty conditionPointProperty() { return conditionPoint; }
+    public DoubleProperty conditionKPointProperty() { return conditionKPoint; }
+    public DoubleProperty generalPointProperty() { return generalPoint; }
+
+    public void setName(String name) { this.name = name; }
     public void setPrice(double price) { this.price = price; }
-    public void setPricePoint(Double point) { this.pricePoint.set(point); }
+    public void setPricePoint(double point) { this.pricePoint.set(point); }
+    public void setPriceKPoint(double point) { this.priceKPoint.set(point); }
+    public void setDays(int days) { this.days = days; }
+    public void setDaysPoint(double point) { this.priceKPoint.set(point); }
+    public void setDaysKPoint(double point) { this.priceKPoint.set(point); }
+    public void setConditionPoint(double point) { this.conditionPoint.set(point); }
+    public void setConditionKPoint(double point) { this.conditionKPoint.set(point); }
+    public void setGeneralPoint(double point) { this.generalPoint.set(point); }
 }
