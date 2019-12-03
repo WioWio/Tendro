3b3ab658a44c6be304cbfd876f5cf801914e9b66
Wioletta Nosenko <violetta.nosenko@gmail.com>
2019-12-03 21:36:13 +0300

    paycondition class ready

--- code/model/PayCondition.java
+++ code/model/PayCondition.java
@@ -6,4 +6,24 @@ public class PayCondition {
     private int factPayPrecent;
     private int factPayDays;
 
+    public void setPayCondition(int prePayPrecent, int prePayDays, int factPayDays){
+        this.prePayPrecent = prePayPrecent;
+        this.prePayDays = prePayDays;
+        this.factPayDays = factPayDays;
+        this.factPayPrecent = 100 - prePayPrecent;
+    }
+
+    public String toString(){
+        String condition="";
+        if (prePayPrecent!=0){
+            condition += prePayPrecent + "% аванс в течение " + prePayDays + "дн.";
+            if (factPayPrecent!=0){
+                condition += ",\n";
+            }
+        }
+        if (factPayPrecent!=0){
+            condition+= factPayPrecent + " по факту в течение " + factPayDays + "дн.";
+        }
+        return condition;
+    }
 }
